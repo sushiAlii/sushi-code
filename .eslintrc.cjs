51c269d1a6b55546965df80ee72b1be3a34baca4
Ali Laut <ali.laut@rakutentravelxchange.com>
2024-05-17 00:10:31 +0800

    Initial eslint and prettier setup configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,11 +4,26 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:prettier/recommended',
+    'prettier',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    'react/jsx-filename-extension': [
+      1,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
+    'react/button-has-type': 'off',
+    'react/react-in-jsx-scope': 'off',
   },
-}
+};
