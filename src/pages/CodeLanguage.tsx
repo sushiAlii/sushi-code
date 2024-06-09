d0fb0835f1a52cf5006ee485a0657dd39e448251
sushiAlii <aliasgar.laut@gmail.com>
2024-06-10 01:11:04 +0800

    feat: use codemirror to add a code editor UI

--- src/pages/CodeLanguage.tsx
+++ src/pages/CodeLanguage.tsx
@@ -1,5 +1,32 @@
-const CodeLanguage = () => {
-  <h1>Code here</h1>;
-};
+import { javascript } from '@codemirror/lang-javascript';
+import { EditorView, basicSetup } from 'codemirror';
+import { useEffect, useRef } from 'react';
+import { useParams } from 'react-router-dom';
+
+function CodeLanguage() {
+  const { language } = useParams();
+  const editorRef = useRef<HTMLElement>(null);
+
+  useEffect(() => {
+    if (editorRef.current) {
+      const codeEditor = new EditorView({
+        doc: '//Welcome to Sushi Code!',
+        extensions: [basicSetup, javascript()],
+        parent: editorRef.current,
+      });
+
+      return () => codeEditor.destroy();
+    }
+  }, []);
+
+  return (
+    <>
+      <section id="code-editor" ref={editorRef} />
+      <section id="terminal">
+        <h1>Terminal</h1>
+      </section>
+    </>
+  );
+}
 
 export default CodeLanguage;
